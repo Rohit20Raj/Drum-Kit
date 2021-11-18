18284a40f185136cdc42ecb6cdb92002632412ff
Rohit Raj <rohitrajvns2020@gmail.com>
2021-11-18 17:23:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,35 +3,35 @@ for (var i = 0; i < 7; i++)
     document.querySelectorAll(".btn")[i].addEventListener("click", handleClick);
 
 function handleClick() {
-
-    // console.log(this.innerHTML);
-    // this.style.color = "red";
-    if (this.innerHTML == "w") {
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-    }
-    if (this.innerHTML == "a") {
-        var audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-    }
-    if (this.innerHTML == "s") {
-        var audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-    }
-    if (this.innerHTML == "d") {
-        var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-    }
-    if (this.innerHTML == "j") {
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
-    }
-    if (this.innerHTML == "k") {
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
-    }
-    if (this.innerHTML == "l") {
-        var audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
+    var buttonClick = this.innerHTML;
+    switch (buttonClick) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
     }
 }
